ca7f68fe84ffdce4936db26075355f95b99d661b
Djenedem8 <djenebouena2008@yahoo.fr>
2025-11-29 20:33:43 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -120,7 +120,7 @@ elif page == '⛷️ Skills':
   st.subheader('Accounting Skills')
 
   skills_data = {
-    'Hard Skill' : 86,
+    'Hard Skill' : 70,
     'Macroenomic' : 80,
     'Accounting' : 90,
     'CIS211' : 90,
